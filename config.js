0b65de6dd4e05f7d87dbc064aa4e81c7c612ac3e
Andrew Kim <andiskim@gmail.com>
2021-03-15 13:15:55 -0700

    Add yarn start

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
 module.exports = {
   BASE_URL: 'https://coinmarketcap.com',
   HEADLESS: true,
-  PAGE_START: 5,
-  NUMBER_OF_PAGES: 5,
+  PAGE_START: 2,
+  NUMBER_OF_PAGES: 10,
   VOLUME_THRESHOLD: 300000,
   PERCENT_THRESHOLD: 15
 }
